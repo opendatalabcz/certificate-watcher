139e78f6319cc375610fe12724b59b8547c12b69
Michal Demko <demkomic@cvut.cz>
2024-03-08 14:00:25 +0100

    [certificate-processor] main update

--- services/certificate-processor/main.py
+++ services/certificate-processor/main.py
@@ -3,7 +3,12 @@ import optparse
 import os
 import sys
 
+from requests.exceptions import ConnectionError, RequestException, SSLError, Timeout
 from src.commons.stream_handler.rabbitmq_stream_handler import RabbitMQStreamHandler
+from src.commons.timer.func_timer import TimerExceptionError
+from src.domain_handler.image_domain_handler import ImageDomainHandler
+from src.domain_handler.string_domain_handler import StringDomainHandler
+from src.scraper.web_scraper import BS4WebScraper
 
 parser = optparse.OptionParser(description="Certificate watcher service to process certificates")
 parser.add_option("-c", "--config-file", metavar="FILENAME", type=str, help="Config file location")
@@ -35,26 +40,54 @@ try:
 
     config = configparser.ConfigParser()
     config.read(args.config_file)
-
-    # CERTSTREAM_URL = config.get("certificate-stream", "certstream_url")
-
 except Exception as e:
     print(f"Fatal error during initialization: {e}")
     sys.exit(1)
 
 # postgres_storage = PostgresStorage(database_connection_info=database_connection_info)
 rabbitmq_handler = RabbitMQStreamHandler(connection_parameters=rabbitmq_connection_info, queue_name="certstream-test")
+# parser should be loaded from config file
+webscraper = BS4WebScraper(parser="lxml", timeout=5)
 
+# config for domain handlers, should be loaded from config file
+domain_handler_config = {"string_domains": ["csob", "moneta", "reiffeisen", "unicredit", "komercni-banka"]}
+string_domain_handler = StringDomainHandler(config=domain_handler_config)
+image_domain_handler = ImageDomainHandler(webscraper=webscraper, config=domain_handler_config)
 
-def callback(ch, method, properties, body):  # noqa: ARG001
-    print(" [x] Received %r" % body)
+# def task_function(domain):
+#     return image_domain_handler.check([domain])
 
 
 def main():
     # data = rabbitmq_handler.receive_single_frame()
     # print(data)
     print(" [*] Waiting for messages. To exit press CTRL+C")
-    rabbitmq_handler.receive_multiple_frames(callback)
+    while True:
+        # rabbitmq_handler.receive_multiple_frames(callback)
+        domain = rabbitmq_handler.receive_single_frame()
+        print(domain)
+
+        str_result = string_domain_handler.check([domain])
+        try:
+            img_result = image_domain_handler.check([domain])
+            # img_result = run_with_timeout(task_function, 5, domain)
+        except TimerExceptionError as e:
+            print("TIMEOUT: ", e)
+            img_result = None
+        except (ConnectionError, SSLError, Timeout, RequestException) as e:
+            print("BIG ERROR: ", e)
+            img_result = None
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            img_result = None
+
+        if str_result:
+            print(str_result)
+        if img_result:
+            print(img_result)
+        else:
+            print("NO RESULT")
+            # print(f"Domain {domain} is not phishing")
 
 
 if __name__ == "__main__":
@@ -65,4 +98,4 @@ if __name__ == "__main__":
         try:
             sys.exit(0)
         except SystemExit:
-            os._exit(0)
+            os.exit(0)
