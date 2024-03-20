0bca82c64dfa1000cf9e3276152e212a36df7997
Michal Demko <demkomic@cvut.cz>
2024-03-20 16:04:19 +0100

    [commons] sqlalchemy db connector and models

---
+++ services/commons/db_storage/models.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from sqlalchemy import JSON, Boolean, DateTime, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql import func
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class FlaggedDomain(Base):
+    __tablename__ = "flagged_data"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    domain: Mapped[str] = mapped_column(String, nullable=False)
+    flagged_domain_base: Mapped[str] = mapped_column(String, nullable=False)
+    algorithm_name: Mapped[str] = mapped_column(String, nullable=False)
+    scraped: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    scraped_images: Mapped[Optional[JSON]] = mapped_column(JSON)
+    last_scraped: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
+
+    def __repr__(self) -> str:
+        return (
+            f"FlaggedDomain(id={self.id!r}, domain={self.domain!r}, "
+            f"algorithm_name={self.algorithm_name!r}, scraped={self.scraped!r}, "
+            f"scraped_images={self.scraped_images!r}, last_scraped={self.last_scraped!r})"
+        )
--- services/commons/db_storage/postgres_storage.py
+++ services/commons/db_storage/postgres_storage.py
@@ -1,8 +1,11 @@
 import os
 
 import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from .abstract_storage import AbstractStorage
+from .models import Base
 from .utils import PostgreSQLConnectionInfo
 
 
@@ -48,3 +51,34 @@ class PostgresStorage(AbstractStorage):
 
             print(f"Postgres error: {e}, query:{query}")
             raise Exception(f"Postgres error: {e}") from e
+
+
+class SqlAlchemyStorage(AbstractStorage):
+    def __init__(self, database_connection_info: PostgreSQLConnectionInfo):
+        super().__init__()
+        self.database_connection_info = database_connection_info
+
+        self.engine = self.__create_connection_engine(self.__create_connection_string(self.database_connection_info))
+
+        self.__init_tables()
+
+    @staticmethod
+    def __create_connection_string(database_connection_info):
+        return f"postgresql+psycopg2://{database_connection_info.username}:{database_connection_info.password}@{database_connection_info.hostname}:{database_connection_info.port}/{database_connection_info.database}"
+
+    @staticmethod
+    def __create_connection_engine(connection_string):
+        return create_engine(connection_string)
+
+    def __init_tables(self):
+        Base.metadata.create_all(self.engine)
+
+    def get_session(self):
+        return sessionmaker(bind=self.engine)()
+
+    def add(self, items: list):
+        with self.get_session() as session:
+            session.add_all(items)
+            session.commit()
+            # session.flush()
+            print(f"Added {len(items)} items to the database")
