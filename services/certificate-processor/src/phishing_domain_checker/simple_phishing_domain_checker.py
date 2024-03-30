b45ff5eef01b5ec6c248092ec20c567500561cae
Michal Demko <demkomic@cvut.cz>
2024-03-30 23:09:39 +0100

    [certificate-processor] simple algorithm refactor

--- services/certificate-processor/src/phishing_domain_checker/simple_phishing_domain_checker.py
+++ services/certificate-processor/src/phishing_domain_checker/simple_phishing_domain_checker.py
@@ -7,20 +7,20 @@ class SimplePhishingDomainChecker(AbstractPhishingDomainChecker):
 
     def __init__(self, settings: dict):
         super().__init__(settings=settings)
-        self.wanted_strings: list = []
+        self.legitimate_strings: list = []
         self.result_company_table: dict = {}
         self.__setup_checker()
 
     def __setup_checker(self):
-        self.logger.info("Setting up SimplePhishingDomainChecker")
+        self.logger.info(f"Setting up {self.__class__.__name__}")
         self.result_company_table = {setting.get("domain"): setting_name for setting_name, setting in self.settings.items()}
-        self.wanted_strings = list(self.result_company_table.keys())
+        self.legitimate_strings = list(self.result_company_table.keys())
 
     def check_domain(self, domain: str) -> str | None:
         if not domain:
             self.logger.warning("No domain provided")
             return None
-        result = [self.result_company_table[wanted_string] for wanted_string in self.wanted_strings if wanted_string in domain]
+        result = [self.result_company_table[wanted_string] for wanted_string in self.legitimate_strings if wanted_string in domain]
         return result[0] if result else None
 
     @staticmethod
