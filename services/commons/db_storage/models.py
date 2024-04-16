86c4586248e6be1aad781e54243506b72d2a3135
Michal Demko <demkomic@cvut.cz>
2024-04-16 16:14:38 +0200

    [commons] database models update

--- services/commons/db_storage/models.py
+++ services/commons/db_storage/models.py
@@ -9,23 +9,6 @@ class Base(DeclarativeBase):
     pass
 
 
-class FlaggedData(Base):
-    __tablename__ = "flagged_data"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    domain: Mapped[str] = mapped_column(String, nullable=False)
-    search_setting_id: Mapped[int] = mapped_column(ForeignKey("search_settings.id"), nullable=False)
-    algorithm: Mapped[str] = mapped_column(String, nullable=False)
-    flagged_time: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now(), nullable=False)
-    images_scraped: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
-    last_scraped: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
-
-    # Relationship to SearchSetting
-    search_setting = relationship("SearchSetting", back_populates="flagged_data", lazy="noload")
-    # Relationship to Image
-    images = relationship("Image", back_populates="flagged_data")
-
-
 class SearchSetting(Base):
     __tablename__ = "search_settings"
 
@@ -54,10 +37,35 @@ class Image(Base):
     name: Mapped[str] = mapped_column(String, nullable=False)
     image_url: Mapped[Optional[str]] = mapped_column(String)
     local_path: Mapped[str] = mapped_column(String, nullable=False)
-    format: Mapped[str] = mapped_column(String, nullable=False)
+    format: Mapped[str] = mapped_column(String, nullable=True)
     created: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
+    note: Mapped[Optional[str]] = mapped_column(String)
 
     # Relationship to FlaggedData
-    flagged_data = relationship("FlaggedData", back_populates="images")
-    # Relationship to SearchSetting for the logo
-    logo_setting = relationship("SearchSetting", back_populates="logo", uselist=False)
+    # flagged_data = relationship("FlaggedData", back_populates="images")
+    # # Relationship to SearchSetting for the logo
+    # logo_setting = relationship("SearchSetting", back_populates="logo", uselist=False)
+
+    flagged_data = relationship("FlaggedData", foreign_keys=[flag_id], back_populates="images")
+    logo_setting = relationship("SearchSetting", foreign_keys=[SearchSetting.logo_id], back_populates="logo")
+
+
+class FlaggedData(Base):
+    __tablename__ = "flagged_data"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    domain: Mapped[str] = mapped_column(String, nullable=False)
+    search_setting_id: Mapped[int] = mapped_column(ForeignKey("search_settings.id"), nullable=False)
+    algorithm: Mapped[str] = mapped_column(String, nullable=False)
+    flagged_time: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now(), nullable=False)
+    images_scraped: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    suspected_logo_found: Mapped[int] = mapped_column(ForeignKey("images.id"), nullable=True)
+
+    last_scraped: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
+
+    # Relationship to SearchSetting
+    search_setting = relationship("SearchSetting", back_populates="flagged_data", lazy="noload")
+
+    # images = relationship("Image", back_populates="flagged_data")
+    images = relationship("Image", foreign_keys=[Image.flag_id], back_populates="flagged_data")
+    suspected_logo = relationship("Image", foreign_keys=[suspected_logo_found], post_update=True)
