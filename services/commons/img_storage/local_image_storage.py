987066ea489fd7406615ab6d983ae30c3e8e7d81
Michal Demko <demkomic@cvut.cz>
2024-04-16 16:00:06 +0200

    [commons] local image storage clear directory

--- services/commons/img_storage/local_image_storage.py
+++ services/commons/img_storage/local_image_storage.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from ..logging.app_logger import AppLogger
 
@@ -29,7 +30,11 @@ class LocalImageStorage(AbstractImageStorage):
         dirpath = f"{self.storage_path}/{path}"
         self.__check_path(dirpath)
         for filename, img_data in data.items():
-            img_data["img"].save(f"{dirpath}/{filename}.{img_data['img'].format.lower()}")
+            if img_data["img"]:
+                img_data["img"].save(f"{dirpath}/{filename}.{img_data['img'].format.lower()}")
+                img_data["saved"] = True
+            else:
+                img_data["saved"] = False
         # with open(path, 'wb') as f:
         #     f.write(data)
 
@@ -38,7 +43,24 @@ class LocalImageStorage(AbstractImageStorage):
             return f.read()
 
     def delete(self, path: str):
-        os.remove(path)
+        os.remove(f"{self.storage_path}/{path}")
+
+    def clear_directory_keep(self, directory: str):
+        # Iterate over all the entries in the directory
+        dirpath = f"{self.storage_path}/{directory}"
+        for entry in os.listdir(dirpath):
+            path = os.path.join(dirpath, entry)
+            # Check if it's a file or a directory
+            if os.path.isdir(path):
+                # It's a directory, remove it and all its contents
+                shutil.rmtree(path)
+            else:
+                # It's a file, remove it
+                os.remove(path)
+        if directory == "":
+            # Create empty .gitkeep file in root of the directory
+            with open(f"{self.storage_path}/.gitkeep", "w") as f:  # noqa
+                pass
 
     def __check_path(self, path: str):
         if not os.path.exists(path):
