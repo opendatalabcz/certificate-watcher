2517afc5744de948691b5e5e5a9695f957dfeeb7
Michal Demko <demkomic@cvut.cz>
2025-01-09 16:08:34 +0100

    [project] run script update

--- run.sh
+++ run.sh
@@ -4,10 +4,17 @@ docker compose up -d rabbitmq
 docker compose up -d postgres
 
 # Wait for RabbitMQ to boot up
+echo "Waiting for RabbitMQ and Postgresql to boot up..."
 sleep 15
+
 
 # Start all other services
 docker compose up -d certificate-stream
-docker compose up -d certificate-processor-simple
+docker compose up -d certificate-processor-domain-str-simple
+docker compose up -d certificate-processor-domain-images
+docker compose up -d periodic-scrape-checker
+
+docker compose up -d backend
+docker compose up -d frontend
 
 echo "All services are up and running"
